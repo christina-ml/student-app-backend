fca7d2990a5b694cd421dbee5973b342a0eddcb9
Christina Loiacono <loiaconochristina@gmail.com>
2022-07-27 14:00:10 -0400

    added update to controller

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -74,6 +74,25 @@ controller.get('/:id/grades', async (req, res) => {
     }
 });
 
+// UPDATE
+controller.put('/:id', async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        const {firstname, lastname, company, city, skill, pic} = req.body;
+        
+        // console.log(req.body)
+        // console.log(firstname)
+
+        const updatedUser = await db.one('UPDATE students SET firstname=$1, lastname=$2, company=$3, city=$4, skill=$5, pic=$6 WHERE id=$7 RETURNING *', [firstname, lastname, company, city, skill, pic, studentId]
+        );
+
+        res.json(updatedUser)
+
+    } catch (err){
+        res.status(500).send(err);
+    }
+})
+
 controller.delete('/:id', async (req, res) => {
     try {
         const studentId = req.params.id;
